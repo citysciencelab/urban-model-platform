c9b5fa359618456f92851b3f7687bf1655087e1f
herzogrh <rico.herzog@outlook.com>
2024-08-05 16:15:42 +0200

    Postgis fixes for dev environment

--- src/ump/initializers/db/create_jobs_table.sql
+++ src/ump/initializers/db/create_jobs_table.sql
@@ -1,11 +1,11 @@
 CREATE TYPE status AS ENUM ('accepted', 'running', 'successful', 'failed', 'dismissed');
 
 CREATE TABLE IF NOT EXISTS jobs (
-  process_id       varchar(40),
-  job_id           varchar(40) PRIMARY KEY,
-  remote_job_id    varchar(40),
-  provider_prefix  varchar(40),
-  provider_url     varchar(40),
+  process_id       varchar(80),
+  job_id           varchar(80) PRIMARY KEY,
+  remote_job_id    varchar(80),
+  provider_prefix  varchar(80),
+  provider_url     varchar(80),
   status           status,
   message          text,
   created          timestamp,
