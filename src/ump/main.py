2bfa46f16a09863ca587598306096e8b10a0658e
Stefan Schuhart <stefan.schuhart@gv.hamburg.de>
2024-08-20 15:15:19 +0000

    removed double entry

--- src/ump/main.py
+++ src/ump/main.py
@@ -84,5 +84,4 @@ def handle_http_exception(error):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=False)
