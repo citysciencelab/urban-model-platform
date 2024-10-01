089fe455aef4ca6b7a66e772b6d882457851b1c2
Andreas Schmitz <schmitz@terrestris.de>
2024-10-01 09:22:58 +0200

    feat: add dynamic reloading of providers.yaml

--- src/ump/api/providers.py
+++ src/ump/api/providers.py
@@ -1,5 +1,8 @@
 import logging
-import traceback
+import os
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
 import aiohttp
 import yaml
@@ -12,6 +15,16 @@ with open(config.PROVIDERS_FILE) as file:
     if content := yaml.safe_load(file):
         PROVIDERS.update(content)
 
+class ProviderLoader(FileSystemEventHandler):
+    def on_modified(event):
+        if event.src_path == config.PROVIDERS_FILE:
+            with open(config.PROVIDERS_FILE) as file:
+                if content := yaml.safe_load(file):
+                    PROVIDERS.update(content)
+
+observer = Observer()
+observer.schedule(ProviderLoader(), os.path.dirname(config.PROVIDERS_FILE), recursive=False)
+observer.start()
 
 def authenticate_provider(p):
     auth = None
