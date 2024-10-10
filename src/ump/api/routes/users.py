b7aee42aef971a37f55628e67a139ee3eb3a4f5f
Kai Volland <kaivolland@yahoo.de>
2024-10-10 11:18:21 +0200

    refactor: get username via keycloak package

--- src/ump/api/routes/users.py
+++ src/ump/api/routes/users.py
@@ -1,33 +1,11 @@
 import json
-from os import environ as env
 
-import requests
 from apiflask import APIBlueprint
-from flask import Response, g, request
-from keycloak import KeycloakAdmin
+from flask import Response, g
+
+from ump.api.keycloak import get_user_name
 
 users = APIBlueprint("users", __name__)
-
-
-def getToken():
-    token_url = f"{env['KEYCLOAK_PROTOCOL']}://{env['KEYCLOAK_HOST']}/auth/realms/master/protocol/openid-connect/token"
-
-    data = {
-        "grant_type": "password",
-        "client_id": "admin-cli",
-        "username": env["KEYCLOAK_USER"],
-        "password": env["KEYCLOAK_PASSWORD"],
-    }
-
-    response = requests.post(token_url, data=data)
-    if response.status_code != 200:
-        print("Fehler beim Abrufen des Tokens:", response.text)
-        exit()
-
-    token_info = response.json()
-    access_token = token_info["access_token"]
-
-    return access_token
 
 
 @users.route("/<path:user_id>/name")
@@ -36,19 +14,7 @@ def index(user_id=None):
     if auth is None:
         return Response(mimetype="application/json", status=401)
 
-    access_token = getToken()
-    user_url = f"{env['KEYCLOAK_PROTOCOL']}://{env['KEYCLOAK_HOST']}/auth/admin/realms/UrbanModelPlatform/users/{user_id}"
-
-    headers = {
-        "Authorization": f"Bearer {access_token}",
-    }
-
-    user_response = requests.get(user_url, headers=headers)
-    if user_response.status_code != 200:
-        return Response(mimetype="application/json", status=404)
-
-    user_info = user_response.json()
-    user_name = user_info.get("username")
+    user_name = get_user_name(user_id)
     response_data = {
         "user_id": user_id,
         "username": user_name,
