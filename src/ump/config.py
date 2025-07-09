31ce8801a0b67b5fe80289466da740639256a9ab
Stefan Schuhart <stefan.schuhart@gv.hamburg.de>
2025-07-09 08:26:38 +0000

    added a dedicated DatabaseConnection

--- src/ump/config.py
+++ src/ump/config.py
@@ -7,17 +7,29 @@ from rich import print
 logger = logging.getLogger(__name__)
 # using pydantic_settings to manage environment variables
 # and do automatic type casting in a central place
-class UmpSettings(BaseSettings):
+class DatabaseConnection(BaseSettings):
+    class Config:
+        extra = "ignore"
+
     UMP_LOG_LEVEL: str = "INFO"
-    UMP_PROVIDERS_FILE: FilePath = "providers.yaml"
-    UMP_API_SERVER_URL: str = "localhost:3000"
-    UMP_API_SERVER_WORKERS: int = 4
-    UMP_REMOTE_JOB_STATUS_REQUEST_INTERVAL: int = 5
     UMP_DATABASE_NAME: str = "ump"
     UMP_DATABASE_HOST: str = "postgres"
     UMP_DATABASE_PORT: int = 5432
     UMP_DATABASE_USER: str = "postgres"
     UMP_DATABASE_PASSWORD: SecretStr = "postgres"
+
+    def print_settings(self):
+        """Prints the settings for debugging purposes"""
+        logger.info("UMP Settings:")
+        print(self)
+
+
+class UmpSettings(DatabaseConnection):
+    UMP_LOG_LEVEL: str = "INFO"
+    UMP_PROVIDERS_FILE: FilePath = "providers.yaml"
+    UMP_API_SERVER_URL: str = "localhost:3000"
+    UMP_API_SERVER_WORKERS: int = 4
+    UMP_REMOTE_JOB_STATUS_REQUEST_INTERVAL: int = 5
     UMP_GEOSERVER_URL: HttpUrl | None = HttpUrl("http://geoserver:8080/geoserver")
     UMP_GEOSERVER_DB_HOST: str = "postgis"
     UMP_GEOSERVER_DB_PORT: int = 5432
