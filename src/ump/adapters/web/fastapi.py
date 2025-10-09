63eae029be50ef4ca386c49e61aba9eb4c41d023
Stefan Schuhart <stefan.schuhart@gv.hamburg.de>
2025-10-09 14:05:35 +0000

    injecting adapter logic

--- src/ump/adapters/web/fastapi.py
+++ src/ump/adapters/web/fastapi.py
@@ -3,27 +3,34 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 
+from ump.core.interfaces.http_client import HttpClientPort
+from ump.core.interfaces.process_id_validator import ProcessIdValidatorPort
 from ump.core.interfaces.providers import ProvidersPort
 from ump.core.exceptions import OGCProcessException
 from ump.adapters.aiohttp_client_adapter import AioHttpClientAdapter
+from ump.adapters.colon_process_id_validator import ColonProcessId
 from ump.core.managers.process_manager import ProcessManager
 
 # Note: this a driver adapter, so it depends on the core interface (ProcessesPort)
 # but the core does not depend on this adapter
 # it does not need to implement a port/interface itself
 # it just uses the interface of the core (ProcessesPort)
-def create_app(provider_config_service: ProvidersPort):
-    
+def create_app(
+    provider_config_service: ProvidersPort,
+    http_client: HttpClientPort,
+    process_id_validator: ProcessIdValidatorPort
+):
+    # must not be injected directly, because it needs to be created in the lifespan
     process_port: ProcessManager | None = None
     
     @asynccontextmanager
     async def lifespan(app: FastAPI):
         nonlocal process_port
 
-        async with AioHttpClientAdapter() as http_client:
+        async with http_client as client:
             process_port = ProcessManager(
-                provider_config_service, http_client,
-                process_id_validator=provider_config_service
+                provider_config_service, client,
+                process_id_validator=process_id_validator
             )
             app.state.process_port = process_port
             yield
