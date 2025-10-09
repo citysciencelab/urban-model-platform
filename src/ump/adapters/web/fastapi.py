38e6164b29ebf28c82641ba6d10548f103502edf
Stefan Schuhart <stefan.schuhart@gv.hamburg.de>
2025-10-09 08:46:06 +0000

    added architectural note note

--- src/ump/adapters/web/fastapi.py
+++ src/ump/adapters/web/fastapi.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from ump.core.interfaces.processes import ProcessesPort
 
+# Note: this a driver adapter, so it depends on the core interface (ProcessesPort)
+# but the core does not depend on this adapter
+# it does not need to implement a port/interface itself
+# it just uses the interface of the core (ProcessesPort)
 def create_app(process_port: ProcessesPort):
 
     app = FastAPI()
