e4588c817f2d1dc4e0deaa45cae02232a8247088
Stefan Schuhart <stefan.schuhart@gv.hamburg.de>
2025-10-09 09:47:44 +0000

    Processes interface and its implementation

--- src/ump/core/interfaces/processes.py
+++ src/ump/core/interfaces/processes.py
@@ -1,7 +1,13 @@
+# ump/core/interfaces/processes.py
 from abc import ABC, abstractmethod
 from ump.core.models.process import ProcessList
 
 class ProcessesPort(ABC):
     @abstractmethod
-    def get_all_processes(self) -> ProcessList:
+    async def get_all_processes(self) -> ProcessList:
+        pass
+    
+    @abstractmethod
+    async def cleanup(self) -> None:
+        """Cleanup resources"""
         pass
--- src/ump/core/managers/process_manager.py
+++ src/ump/core/managers/process_manager.py
@@ -1,29 +1,32 @@
-import httpx
+# ump/core/managers/process_manager.py
 from ump.core.interfaces.processes import ProcessesPort
 from ump.core.interfaces.providers import ProvidersPort
+from ump.core.interfaces.http_client import HttpClientPort
 from ump.core.models.process import Process, ProcessList
 
 class ProcessManager(ProcessesPort):
-    def __init__(self, provider_config_service: ProvidersPort) -> None:
+    def __init__(
+        self, 
+        provider_config_service: ProvidersPort,
+        http_client: HttpClientPort
+    ) -> None:
         self.provider_config_service = provider_config_service
+        self.http_client = http_client
 
-    def get_all_processes(self) -> ProcessList:
-
+    async def get_all_processes(self) -> ProcessList:
         all_processes = []
 
         for provider_name in self.provider_config_service.list_providers():
-            
             provider = self.provider_config_service.get_provider(provider_name)
-            
             url = str(provider.server_url).rstrip("/") + "/processes"
             
-            response = httpx.get(url, timeout=provider.timeout)
-            
-            response.raise_for_status()
-            
-            data = response.json()
+            data = await self.http_client.get(url, timeout=provider.timeout)
             
             for proc in data.get("processes", []):
                 all_processes.append(Process(**proc))
         
         return ProcessList(processes=all_processes)
+    
+    async def cleanup(self) -> None:
+        """Cleanup resources"""
+        await self.http_client.close()
