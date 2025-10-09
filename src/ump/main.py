a70e09779ecf2629294ee0d70c900e40e16a9c1a
Stefan Schuhart <stefan.schuhart@gv.hamburg.de>
2025-10-09 09:49:09 +0000

    updated main

--- src/ump/main.py
+++ src/ump/main.py
@@ -1,31 +1,28 @@
-# Main entrypoint for UMP app
+# main.py
 import os
 
 import uvicorn
 
 from ump.adapters.provider_config_file_adapter import ProviderConfigFileAdapter
 from ump.adapters.web.fastapi import create_app
-from ump.core.managers.process_manager import ProcessManager
 from ump.core.settings import logger
 
+# main live at the outermost layer (not in core)
+# Instantiates all the concrete adapters
+# Wires dependencies together
+# Starts the application
 
 def main():
-    # Path to providers.yaml (adjust as needed)
     config_path = os.path.join(os.path.dirname(__file__), "../../providers.yaml")
     config_path = os.path.abspath(config_path)
 
-    # Instantiate provider config adapter
     provider_config_adapter = ProviderConfigFileAdapter(config_path)
     provider_config_adapter.start_file_watcher()
 
-    # Instantiate process manager (implements ProcessesPort)
-    process_manager = ProcessManager(provider_config_adapter)
-
-    # Create FastAPI app, injecting process_manager
     logger.info("Starting UMP FastAPI application...")
-    app = create_app(process_manager)
-
-    # Run app (FastAPI)
+    
+    # Create app with lifespan that manages the http client
+    app = create_app(provider_config_adapter)
 
     uvicorn.run(app, host="0.0.0.0", port=8000)
 
