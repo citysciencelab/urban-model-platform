0c2e15cdde1019c32d68e8e2a9b6dec47fa57ac9
Stefan Schuhart <stefan.schuhart@gv.hamburg.de>
2025-10-09 14:16:00 +0000

    injecting additional adapters

--- src/ump/main.py
+++ src/ump/main.py
@@ -1,13 +1,15 @@
 # main.py
 import os
 
+from ump.adapters.aiohttp_client_adapter import AioHttpClientAdapter
+from ump.adapters.colon_process_id_validator import ColonProcessId
 import uvicorn
 
 from ump.adapters.provider_config_file_adapter import ProviderConfigFileAdapter
 from ump.adapters.web.fastapi import create_app
 from ump.core.settings import logger
 
-# main live at the outermost layer (not in core)
+# main lives at the outermost layer (not in core)
 # Instantiates all the concrete adapters
 # Wires dependencies together
 # Starts the application
@@ -22,7 +24,12 @@ def main():
     logger.info("Starting UMP FastAPI application...")
     
     # Create app with lifespan that manages the http client
-    app = create_app(provider_config_adapter)
+    # injecting necessary dependencies
+    app = create_app(
+        provider_config_adapter,
+        AioHttpClientAdapter(),
+        ColonProcessId()
+    )
 
     uvicorn.run(app, host="0.0.0.0", port=8000)
 
