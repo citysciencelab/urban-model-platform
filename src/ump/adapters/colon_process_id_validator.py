26ed7759354d047dfcacb5d90310d0899dbfb592
Stefan Schuhart <stefan.schuhart@gv.hamburg.de>
2025-10-17 11:05:40 +0000

    added handling of colon based ids

--- src/ump/adapters/colon_process_id_validator.py
+++ src/ump/adapters/colon_process_id_validator.py
@@ -17,4 +17,14 @@ class ColonProcessId(ProcessIdValidatorPort):
         return match.group(1), match.group(2)
 
     def create(self, provider_prefix: str, process_id: str) -> str:
+
+        # if the remote server uses ":" in its process IDs
+        # we need to respect that
+        try:
+            possible_process_id_with_prefix = process_id
+            _, process_id = self.extract(possible_process_id_with_prefix)
+        except ValueError:
+            # if not, do nothing special
+            pass
+
         return f"{provider_prefix}:{process_id}"
